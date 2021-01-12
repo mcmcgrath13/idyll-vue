5a7f88438c13fbadb338415edc4c67ee6edd7692
mcmcgrath13 <m.c.mcgrath13@gmail.com>
2021-01-12 11:03:51 -0500

    feat: rendering hello world document with react

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Idyll file="hello_world"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Idyll from './components/Idyll.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Idyll
   }
 }
 </script>
---
+++ src/components/Idyll.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="idyll"></div>
+</template>
+
+<script>
+import React from 'react'; // eslint-disable-line
+import ReactDOM from 'react-dom';
+import IdyllDocument from 'idyll-document';
+import * as components from 'idyll-components';
+import {toRefs, onMounted} from 'vue';
+import idyllMarkup from '../assets/idyll/hello_world.idyll';
+
+// You must provide idyllMarkup
+// and the container element (a DOM node).
+export default {
+  props: {
+    file: {
+      type: String,
+      required: true
+    }
+  },
+  setup (props) {
+    const { file } = toRefs(props);
+    console.log(file);
+
+    console.log(idyllMarkup);
+
+    onMounted( () => {
+      ReactDOM.render(
+        React.createElement(IdyllDocument, {markup: idyllMarkup, components, datasets: {}}, null),
+        document.getElementById('idyll')
+      )
+    })
+
+    return {
+      idyllMarkup
+    }
+  }
+}
+
+</script>
